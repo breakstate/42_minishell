736b6fc905a612555437a30f3d659b6c53c0c942
Brenton MOODLEY <bmoodley@e4r15p10.wtc.co.za>
2017-07-29 15:28:37 +0200

    added rough implementation of echo

--- built_ins.c
+++ built_ins.c
@@ -1,14 +1,77 @@
 #include "minishell.h"
 
-int		ft_cd(char *arg)
+int		ft_cd(t_pack *pack)
 {
-	printf("arg = %s\n", arg);
-	if (!(chdir(arg) == 0))
+	if ((chdir(pack->args[1]) == -1))
 		return (-1);
 	return (0);
 }
 
-void	ft_exit(void)
+int		ft_echo(t_pack *pack)
 {
-	exit(0);
+	int		i;
+	int		q_flag;
+
+	i = 0;
+	q_flag = 0;
+	while (ft_isspace(pack->cmd[i]))
+		i++;
+	while (!(ft_isspace(pack->cmd[i])))
+		i++;
+	while (ft_isspace(pack->cmd[i]))
+		i++;
+	while (pack->cmd[i])
+	{
+		if (pack->cmd[i] == '"' && pack->cmd[i - 1] != 92 && q_flag == 0)
+		{
+			q_flag = 1;
+			i++;
+		}
+		else if (pack->cmd[i] == '"' && pack->cmd[i - 1] != 92 && q_flag == 1)
+		{
+			q_flag = 0;
+			i++;
+		}
+		if (q_flag == 1)
+			ft_putchar(pack->cmd[i]);
+		else 
+		{
+			if (!(ft_isspace(pack->cmd[i])))
+				ft_putchar(pack->cmd[i]);
+			else if (pack->cmd[i] != ' ' && pack->cmd[i - 1] == ' ')
+			{
+
+				ft_putchar(' ');
+			}
+		}
+		printf("|i = %c, i - 1 = %c|\n", pack->cmd[i], pack->cmd[i - 1]);
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
 }
+/*
+int		ft_echo(t_pack *pack)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	j = 0;
+	while(pack->args[i])
+	{
+		while (pack->args[i][j])
+		{
+			if (pack->args[i][j] != '"')
+				ft_putchar(pack->args[i][j]);
+			else if (pack->args[i][j] == '"' && j > 0 && pack->args[i][j - 1] == 92)
+				ft_putchar(92);
+			j++;
+		}
+		ft_putchar('\n');
+		j = 0;
+		i++;
+	}
+	return (0);
+}
+*/
--- launcher.c
+++ launcher.c
@@ -17,40 +17,33 @@ int		is_built_in(char **args)
 	while (builtin[i])
 	{
 		if ((ft_strcmp(args[0], builtin[i]) == 0))
-		{
-			printf("found match\n");
 			ret = i + 1;
-		}
-		printf("args[0] = %s, builtin[%d] = %s\n", args[0], i, builtin[i]);//remove
 		i++;
 	}
 	if (ret == 0)
 		ret = 5;
-	printf("ret = %d\n", ret);//remove
 	return (ret);
 }
 
-//int echo(int **)
-void	b_launch(char **args, int i)
+void	b_launch(t_pack *pack, int i)
 {
-	int (*foo[5])(char *);
+	int (*foo[5])(t_pack *);
+
 	foo[0] = &ft_cd;
-   	(*foo[i])(args[1]);
-	//foo[1] = &ft_echo;
+	foo[1] = &ft_echo;
 	//foo[2] = &ft_setenv;
 	//foo[3] = &ft_unsetenv;
 	//foo[4] = &ft_env;
-
-
+   	(*foo[i])(pack);
 }
 
-void	launcher(char **args)
+void	launcher(t_pack *pack)
 {
 	int		i;
 
 	i = 0;
-	if ((i = is_built_in(args)) < 5)
-		b_launch(args, i - 1);
-	else
-		printf("not built_in\n");
+	if ((i = is_built_in(pack->args)) < 5)
+		b_launch(pack, i - 1);
+	//else---
+		//printf("not built_in\n");---
 }
